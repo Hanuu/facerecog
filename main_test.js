687b0b921a038c97ca1d188fa7e9efa62a0d09a7
minjun kwak <kreateark@gmail.com>
2018-03-21 23:09:07 +0900

    fix: Callibaration focused on carrying

--- main_test.js
+++ main_test.js
@@ -6,15 +6,36 @@ var http = require("http");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+
 app.get('/status', function(req, res){
 	var json = {
-	name : "hi",
-	carry: 0
-	}
+		name : "hi",
+		carry: 0
+		};
+	//To run python code & run 
+	var spawn = require('child_process').spawn
+		ls = spawn('python', ['gyro_carry.py']);
+	
+	ls.stderr.on('data', function(data){
+		console.log('stderr: '+ data);
+	});
+	
+	ls.stdout.on('data', function(data){
+		console.log('stdout: '+ data);
+		var str = data.toString().trim();
+		json = {
+			name : "hi",
+			carry: str
+		}
+		
+		res.json(json)
+		res.end()
+		console.log("send On status to server");
+	});
+	
 
-	res.json(json)
-	res.end()
-	console.log("send On status to server");
+
 });
 
 app.listen(3000, function(){
